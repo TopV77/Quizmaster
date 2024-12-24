01f909c1f00bd76df0b6c5027f7c29c426f6b1dd
TopV77 <159802725+TopV77@users.noreply.github.com>
2024-12-24 14:17:43 +0100

    ui mobile responsivity updates

--- frontend/src/app/pages/favorites/favorites.component.ts
+++ frontend/src/app/pages/favorites/favorites.component.ts
@@ -1,13 +1,8 @@
 import {Component} from '@angular/core';
-import {QuestionShort} from '../../interfaces/questionShort';
-import {QuestionService} from '../../services/question/question.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
-export class FavoritesComponent {
-
-
-}
+export class FavoritesComponent {}
